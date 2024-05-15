4e2e31efe00e54c6fec8b9f54d0d4d0c6fa96f19
frun36 <franek.urbanski@gmail.com>
2024-05-15 20:52:21 +0200

    Playing around with ADC

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -3,12 +3,12 @@
 
 use core::f32::consts::PI;
 
-use panic_halt as _;
+use panic_probe as _;
 
-use rp_pico::hal::{self, clocks::Clock, pac};
-
-use defmt_rtt as _;
 use defmt::info;
+use defmt_rtt as _;
+
+use rp_pico::hal::{self, adc::AdcPin, clocks::Clock, pac, Adc};
 
 use oscilloscope_graphics::display::Display;
 use oscilloscope_graphics::drawable::parametric_path::ParametricPath;
@@ -17,35 +17,45 @@ const TOP: u16 = 1024;
 
 #[rp_pico::entry]
 fn main() -> ! {
-    let mut pac = pac::Peripherals::take().unwrap();
+    let mut peripherals = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
 
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let mut watchdog = hal::Watchdog::new(peripherals.WATCHDOG);
 
     // By default a 125 MHz system clock
     let clocks = hal::clocks::init_clocks_and_plls(
         rp_pico::XOSC_CRYSTAL_FREQ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
+        peripherals.XOSC,
+        peripherals.CLOCKS,
+        peripherals.PLL_SYS,
+        peripherals.PLL_USB,
+        &mut peripherals.RESETS,
         &mut watchdog,
     )
     .unwrap();
 
-    let sio = hal::Sio::new(pac.SIO);
+    let sio = hal::Sio::new(peripherals.SIO);
     let pins = hal::gpio::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
+        peripherals.IO_BANK0,
+        peripherals.PADS_BANK0,
         sio.gpio_bank0,
-        &mut pac.RESETS,
+        &mut peripherals.RESETS,
     );
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
+    // Init ADC
+    let mut adc = Adc::new(peripherals.ADC, &mut peripherals.RESETS);
+    let mut pot = AdcPin::new(pins.gpio26.into_floating_input()).unwrap();
+
+    let mut adc_fifo = adc
+        .build_fifo()
+        .clock_divider(0, 0) // sample as fast as possible (500ksps)
+        .set_channel(&mut pot)
+        .start();
+
     // Init PWMs
-    let mut pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+    let mut pwm_slices = hal::pwm::Slices::new(peripherals.PWM, &mut peripherals.RESETS);
 
     let pwm = &mut pwm_slices.pwm1;
     pwm.set_ph_correct();
@@ -62,6 +72,7 @@ fn main() -> ! {
     let mut display = Display::new(channel_x, channel_y, TOP, -4., 4., -3., 3., &mut delay);
 
     let mut u = 0.;
+    let mut r = 2.;
 
     info!("Hellou");
 
@@ -71,15 +82,22 @@ fn main() -> ! {
             let theta1 = 0.25 * PI * (2 * i + 1) as f32 + u;
             let (sin0, cos0) = libm::sincosf(theta0);
             let (sin1, cos1) = libm::sincosf(theta1);
-            let seg0 = ParametricPath::segment((0., 0.), (2. * cos0, 2. * sin0), 0.1, 1000, 1, 0);
-            let arc = ParametricPath::arc((0., 0.), 2., theta0, 0.1, theta1, 1000, 1, 0);
-            let seg1 = ParametricPath::segment((2. * cos1, 2. * sin1), (0., 0.), -0.1, 1000, 1, 0);
+            let seg0 = ParametricPath::segment((0., 0.), (r * cos0, r * sin0), 0.1, 1000, 1, 0);
+            let arc = ParametricPath::arc((0., 0.), r, theta0, 0.1, theta1, 1000, 1, 0);
+            let seg1 = ParametricPath::segment((r * cos1, r * sin1), (0., 0.), -0.1, 1000, 1, 0);
 
             display.draw(&seg0);
             display.draw(&arc);
-            display.draw(&seg1)
+            display.draw(&seg1);
         }
 
         u += 0.05;
+
+        if adc_fifo.len() > 0 {
+            let adc_counts: u16 = adc_fifo.read();
+            r = 1.5 + adc_counts as f32 / 4096.;
+        }
+
+        watchdog.feed();
     }
 }
